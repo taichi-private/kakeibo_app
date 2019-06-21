337c17812519ebfb2a38ea2acf2cbacc7060aae9
渡辺　太一 <taichi.watanabe@theport.jp>
2019-06-21 15:49:20 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import App from './javascripts/components/App' // 作ったやつ
+
+new Vue({
+  el: '#app', // アプリをマウントする要素(セレクタで指定)
+  components: { App }, // Appというコンポーネントを使うよ、という宣言
+  template: '<app/>', // el(今回は#app)の中に表示する内容
+})
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  // エントリポイントのファイル
+  entry: './src/index.js',
+  output: {
+    // 出力先のディレクトリ
+    path: path.resolve(__dirname, './dest'),
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, // ファイルが.vueで終われば...
+        loader: 'vue-loader' // vue-loaderを使う
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'] // css-loader -> vue-style-loaderの順で通していく
+      }
+    ]
+  },
+  resolve: {
+    // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
+    extensions: ['.js', '.vue'],
+    alias: {
+      // vue-template-compilerに読ませてコンパイルするために必要
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    // webpackの扱わないファイル(HTMLや画像など)が入っているディレクトリ
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 3004, // ポート番号
+  }
+}
